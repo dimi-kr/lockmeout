c91cc7505daf40899dc221a8186d0f5c3f8a43d5
Dimitri Krivonogov <me@dimitri.kr>
2017-07-25 16:26:38 +0200

    Initial Commit

---
+++ logmeout/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  logmeout
+//
+//  Created by Dimetrio on 25.07.17.
+//  Copyright © 2017 smartapps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
